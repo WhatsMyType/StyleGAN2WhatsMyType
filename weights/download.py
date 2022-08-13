f126a1011de5433274c404a3708671d0d8224a71
WhatsMyType <111205198+WhatsMyType@users.noreply.github.com>
2022-08-13 18:09:07 -0500

    Remove unecessary downloads

--- weights/download.py
+++ weights/download.py
@@ -6,7 +6,7 @@ import gdown
 file_id = ['1afMN3e_6UuTTPDL63WHaA0Fb9EQrZceE', '1Av4p3JNWkmWsJx6s9Hq32JvlNWSW4kgk',
 	   '1LCyHcKtqNs8NirJeSPq3fKAUZ-MLt_-8', '1pEAeJTK_ZPfkUvV7VFDHq0O4tKYsslkX', 
 	   '16_QtKS2w9-40uxGZ02h3OtFhnJgNgdi_']
-name = ['ffhq.npy', 'car.npy', 'cat.npy', 'church.npy', 'horse.npy']
+name = ['ffhq.npy'] #, 'car.npy', 'cat.npy', 'church.npy', 'horse.npy']
 
 weight_dir = './'
 for i in range(len(name)):
